50f52448bcde9397e361b3a161a9f442e5616c23
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-07-21 17:29:21 +0400

    Updates

---
+++ precache-manifest.89fd3532ab9905bf838c056733414f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547547a710ab7a71e30d607db2d8812e",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "d3cec2b5e6d4184a4f55",
+    "url": "/weather-app-react/static/css/main.3dac8f22.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "/weather-app-react/static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "d3cec2b5e6d4184a4f55",
+    "url": "/weather-app-react/static/js/main.cf1abc48.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "11967fb921076e5d27c8823c5542a154",
+    "url": "/weather-app-react/static/media/cloud.11967fb9.jpg"
+  }
+]);
